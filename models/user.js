4f45a75b7ab4332bce24dbf2566f4a9f116c0731
Siddharth Subramanian <sidsbrmnn@gmail.com>
2019-07-24 00:43:06 +0530

    models: Added user schema and model

---
+++ models/user.js
@@ -0,0 +1,24 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  photo: {
+    type: String
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+const User = model("User", userSchema);
+
+module.exports = User;
