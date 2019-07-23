64a19d26fec9ef3e1de65aa1a7ab1fa6945228a2
Siddharth Subramanian <sidsbrmnn@gmail.com>
2019-07-23 21:17:27 +0530

    routes: Added GET /api/tours

--- routes/tours.js
+++ routes/tours.js
@@ -4,4 +4,10 @@ const Tour = require("../models/tour");
 
 const router = express.Router();
 
+router.get("/", async (req, res) => {
+  const tours = await Tour.find({}).sort("name");
+
+  res.send(tours);
+});
+
 module.exports = router;
