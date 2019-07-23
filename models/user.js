50b8b9f06467cde10ab8cff57aace8471cf80f3e
Siddharth Subramanian <sidsbrmnn@gmail.com>
2019-07-24 00:55:54 +0530

    models: User schema refactored

--- models/user.js
+++ models/user.js
@@ -1,22 +1,10 @@
 const { Schema, model } = require("mongoose");
 
 const userSchema = new Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  photo: {
-    type: String
-  },
-  password: {
-    type: String,
-    required: true
-  }
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  photo: { type: String },
+  password: { type: String, required: true }
 });
 
 const User = model("User", userSchema);
