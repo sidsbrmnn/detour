1524f3f25be34ea7e62c5d26de351459441a4c8c
Siddharth Subramanian <sidsbrmnn@gmail.com>
2019-07-23 20:29:57 +0530

    startup: Other middleware and route definitions

---
+++ startup/routes.js
@@ -0,0 +1,9 @@
+const { json } = require("express");
+const tours = require("../routes/tours");
+const users = require("../routes/users");
+
+module.exports = function(app) {
+  app.use(json());
+  app.use("/api/tours", tours);
+  app.use("/api/users", users);
+};
