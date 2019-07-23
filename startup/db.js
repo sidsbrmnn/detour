44ca34645c4a3c7e1de990ff8164ad9b54c293f7
Siddharth Subramanian <sidsbrmnn@gmail.com>
2019-07-23 22:26:47 +0530

    startup: Modified options for mongoose.connect

--- startup/db.js
+++ startup/db.js
@@ -5,6 +5,6 @@ const DB_URI = process.env.DB_URI;
 module.exports = function() {
   connect(
     DB_URI,
-    { useNewUrlParser: true, useCreateIndex: true }
+    { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false }
   ).then(() => console.info("Connected to MongoDB"));
 };
