79c1b95f4fcc7d9906da7e8420f8051f082fec62
Siddharth Subramanian <sidsbrmnn@gmail.com>
2019-07-24 00:56:49 +0530

    routes: signup route added for user

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,21 @@
 const express = require("express");
 
+const asyncHandler = require("../middlewares/async");
+const { User, validateUser } = require("../models/user");
+
 const router = express.Router();
+
+router.post(
+  "/signup",
+  asyncHandler(async (req, res) => {
+    const { error } = validateUser(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const user = new User(req.body);
+    await user.save();
+
+    res.send(user);
+  })
+);
 
 module.exports = router;
