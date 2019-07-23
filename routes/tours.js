143f9b423e59304cd3ac6d873831aaf9927c61f1
Siddharth Subramanian <sidsbrmnn@gmail.com>
2019-07-24 00:42:11 +0530

    routes: req.body validation added for POST and PATCH

--- routes/tours.js
+++ routes/tours.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const asyncHandler = require("../middlewares/async");
 const { objectId } = require("../middlewares/validate");
-const Tour = require("../models/tour");
+const { Tour, validateTour } = require("../models/tour");
 
 const router = express.Router();
 
@@ -18,6 +18,9 @@ router.get(
 router.post(
   "/",
   asyncHandler(async (req, res) => {
+    const { error } = validateTour(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
     let tour = await Tour.findOne({ name: req.body.name });
     if (tour)
       return res.status(400).send("Tour with the given name already exists.");
@@ -33,6 +36,9 @@ router.patch(
   "/:id",
   objectId,
   asyncHandler(async (req, res) => {
+    const { error } = validateTour(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
     const tour = await Tour.findOneAndUpdate({ _id: req.params.id }, req.body, {
       new: true
     });
