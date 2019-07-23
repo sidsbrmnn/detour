b3de3a291154ae471bb56055c9e0961266833d0f
Siddharth Subramanian <sidsbrmnn@gmail.com>
2019-07-23 20:27:36 +0530

    index: Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+
+require("dotenv").config();
+require("./startup/config")();
+if (process.env.NODE_ENV === "production") require("./startup/prod")(app);
+require("./startup/db")();
+require("./startup/routes")(app);
+
+const port = process.env.PORT;
+app.listen(port, () => console.info(`Listening on port ${port}`));
