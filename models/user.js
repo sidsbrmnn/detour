97b2bcde94f71f177ccc99fb7187848bd1809523
Siddharth Subramanian <sidsbrmnn@gmail.com>
2019-07-24 00:56:21 +0530

    models: Joi validation added for user

--- models/user.js
+++ models/user.js
@@ -1,3 +1,4 @@
+const Joi = require("@hapi/joi");
 const { Schema, model } = require("mongoose");
 
 const userSchema = new Schema({
@@ -7,6 +8,22 @@ const userSchema = new Schema({
   password: { type: String, required: true }
 });
 
+function validateUser(user) {
+  const schema = {
+    name: Joi.string()
+      .min(6)
+      .max(20)
+      .required(),
+    email: Joi.string()
+      .email()
+      .required(),
+    photo: Joi.string(),
+    password: Joi.string().required()
+  };
+
+  return Joi.validate(user, schema);
+}
+
 const User = model("User", userSchema);
 
-module.exports = User;
+module.exports = { User, validateUser };
